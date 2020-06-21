c3712991826eabdae6f340e01601893125fc9f5f
AbrahamRH <abraham.esc.tra@gmail.com>
2020-06-21 16:15:32 -0500

    Se creo el archivo de inicio de sesion y las carpetas de los archivos

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>El descanso medieval</title>
+</head>
+<body>
+    <header>
+        <span>EL DESCANSO MEDIEVAL</span>
+    </header>
+    <main>
+        <h1>Iniciar sesión</h1>
+        <form>
+            <label for="correo">Correo:</label>
+            <input type="text" id=="correo" placeholder="Correo electronico">
+            <label for="password">Contraseña:</label>
+            <input type="password" id="password" placeholder="Contraseña">
+        </form>
+    </main>
+</body>
+</html>
