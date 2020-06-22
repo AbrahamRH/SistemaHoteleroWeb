5786fa1d42454fe397ba1fe723f7683a0af9a79e
AbrahamRH <abraham.esc.tra@gmail.com>
2020-06-22 16:07:29 -0500

    Se crearon contenedores para los formularios y se elimino el footer

--- Pantallas/registroUsuario.php
+++ Pantallas/registroUsuario.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="../CSS/styles.css">
+    <link rel="stylesheet" href="../CSS/registroUsuario.css">
     <title>El descanso medieval</title>
 </head>
 <body>
@@ -13,54 +14,53 @@
         <?php ?>
     </header>
     <main>
-        <h2 class="titulo">Registro de Usuario</h2>
         <form class="registroUsuario" action="POST">
+            <h2 class="titulo">Registro de Usuario</h2>
+            <div class="contenedor">
+                <div class="form-group row">
+                    <label for="nombre" class="col-sm-2 col-form-label">Nombre:</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="nombre" placeholder="Nombre">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="ApPaterno" class="col-sm-2 col-form-label">Apellido paterno:</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="ApPaterno" placeholder="Apellido paterno">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="ApMaterno" class="col-sm-2 col-form-label">Apellido materno:</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="ApMaterno" placeholder="Apellido materno">
+                    </div>
+                </div>
+                
+                <div class="form-group row">
+                    <label for="inputEmail" class="col-sm-2 col-form-label">Correo electrónico:</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" id="inputEmail" placeholder="Correo electrónico">
+                    </div>
+                </div>   
+    
+                <div class="form-group row">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Contraseña:</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" id="inputPassword" placeholder="Contraseña">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="verPassword" class="col-sm-2 col-form-label">Verificar ontraseña:</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" id="verPassword" placeholder=" Verificar contraseña">
+                    </div>
+                </div>
+                <button class="btn-enviar">Enviar</button>
+            </div>
 
-            <div class="form-group row">
-                <label for="nombre" class="col-sm-2 col-form-label">Nombre:</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="nombre" placeholder="Nombre">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="ApPaterno" class="col-sm-2 col-form-label">Apellido paterno:</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="ApPaterno" placeholder="Apellido paterno">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="ApMaterno" class="col-sm-2 col-form-label">Apellido materno:</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="ApMaterno" placeholder="Apellido materno">
-                </div>
-            </div>
-            
-            <div class="form-group row">
-                <label for="inputEmail" class="col-sm-2 col-form-label">Correo electrónico:</label>
-                <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail" placeholder="Correo electrónico">
-                </div>
-            </div>   
-
-            <div class="form-group row">
-                <label for="inputPassword" class="col-sm-2 col-form-label">Contraseña:</label>
-                <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword" placeholder="Contraseña">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="verPassword" class="col-sm-2 col-form-label">Verificar ontraseña:</label>
-                <div class="col-sm-10">
-                    <input type="password" class="form-control" id="verPassword" placeholder=" Verificar contraseña">
-                </div>
-            </div>
-
-
-            <button class="btn-enviar">Enviar</button>
 
         </form>
     </main>
-    <footer></footer>  
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -15,14 +15,15 @@
     <main>
         <form class="form-signin" action="POST">
             <h2 class="titulo">Iniciar sesión</h2>
-            <label for="inputEmail" class="sr-only">Correo electrónico</label>
-            <input type="email" id="inputEmail" class="form-control" placeholder="Correo electrónico" required autofocus>
-            <label for="inputPassword" class="sr-only">Contraseña</label>
-            <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
-            <button class="btn-enviar">Enviar</button>
+            <div class="contenedor">
+                <label for="inputEmail" class="sr-only">Correo electrónico</label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Correo electrónico" required autofocus>
+                <label for="inputPassword" class="sr-only">Contraseña</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+                <button class="btn-enviar">Enviar</button>
+            </div>
         </form>
     </main>
-    <footer></footer>  
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
