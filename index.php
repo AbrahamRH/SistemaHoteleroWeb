73c5bb155c336bd972053a718e39e3d6717eacc4
AbrahamRH <abraham.esc.tra@gmail.com>
2020-06-21 20:23:30 -0500

    Se añadio nuevo archivo de estilos y diseño responsivo al header

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./CSS/styles.css">
+    <link rel="stylesheet" href="./CSS/signin.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <title>El descanso medieval</title>
 </head>
 <body>
     <header>
-        <span>EL DESCANSO MEDIEVAL</span>
+        <div class="logo">EL DESCANSO MEDIEVAL</div>
     </header>
     <main>
         <form class="form-signin">
