ea0f9854a3e54269efc52d2f2ed0b6f8aa3c9fdf
AbrahamRH <abraham.esc.tra@gmail.com>
2020-06-23 23:19:01 -0500

    Se agrego stilos a la pagina de menu

--- Pantallas/menu.php
+++ Pantallas/menu.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/styles.css">
+    <link rel="stylesheet" href="../CSS/menu.css">
     <title>El descanso medieval</title>
 </head>
 <body>
-    
+    <header>
+        <div class="logo">EL DESCANSO MEDIEVAL</div>
+        <?php ?>
+    </header>
+    <main>
+        <div class="flex-container">
+            <a href="#">Registro usuario</a>
+            <a href="#">Reporte ventas</a>
+            <a href="#">Reporte de habitaciones</a>
+            <a href="#">Actualización de usuarios</a>
+            <a href="#">Actualización de servicios</a>
+            <a href="#">Actualización de datos de habitaciones</a>
+            <a href="#">Reservar una habitación</a>
+            <a href="#">Configuración</a>
+        </div>
+    </main>
 </body>
 </html>
