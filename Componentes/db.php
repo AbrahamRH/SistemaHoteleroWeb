54167e9fcd2492fd110f58a56d623c57af8c3aa6
AbrahamRH <abraham.esc.tra@gmail.com>
2020-06-24 14:28:13 -0500

    Se logro hacer el login

--- Componentes/db.php
+++ Componentes/db.php
@@ -6,7 +6,6 @@
     
     try {
         $con = new PDO("mysql:host=$host;dbname=$db;",$user,$password);
-        echo "Entro";
     } catch(PDOException $e){
         die('Connection failed'.$e->getMessage());
     }
--- index.php
+++ index.php
@@ -1,20 +1,31 @@
-<?php
-    include_once './Componentes/db.php';
-
-    session_start();
-
-
-    if (isset($_SESSION['usuario_id'])) {
-        $records = $conn->prepare('SELECT usuario_id, correo, password FROM USUARIO WHERE id = :id');
-        $records->bindParam(':id', $_SESSION['user_id']);
-        $records->execute();
-        $results = $records->fetch(PDO::FETCH_ASSOC);
-
-        $user = null;
-
-        if (count($results) > 0) {
-        $user = $results;
-        }
-    }
-
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/styles.css">
+    <link rel="stylesheet" href="./CSS/login.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>El descanso medieval</title>
+</head>
+<body>
+    <header>
+        <div class="logo">EL DESCANSO MEDIEVAL</div>
+    </header>
+    <main>
+        <form class="form-signin" action="./Pantallas/login.php" method="POST">
+            <h2 class="titulo">Iniciar sesión</h2>
+            <div class="contenedor">
+                <label for="inputEmail" class="sr-only">Correo electrónico</label>
+                <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Correo electrónico" required autofocus>
+                <label for="inputPassword" class="sr-only">Contraseña</label>
+                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+                <button class="btn-enviar">Enviar</button>
+            </div>
+        </form>
+    </main>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
